1d1bd37b81f04f50177cd748f380894222d69ce3
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-29 23:57:52 +0530

    feat: implement routing for Login and Dashboard components

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dash" element={<Dashboard/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
