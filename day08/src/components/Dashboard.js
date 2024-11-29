c812203a036eb07b3949756b40e51207758b775f
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-29 23:57:58 +0530

    feat: add navigation and routing to Dashboard component

--- day08/src/components/Dashboard.js
+++ day08/src/components/Dashboard.js
@@ -1,7 +1,31 @@
-export default function Dashboard(){
+import Home from './Home';
+import ContactUs from './ContactUs';
+import AboutUs from './AboutUs';
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+export default function Dashboard() {
     return (
         <div>
-            <h1>This is the Dashboard</h1>
+            <h1>Hello User</h1>
+            <div>
+
+                <nav>
+                    <ul>
+                        <li><Link to="home">Home</Link></li>
+                        <li><Link to="about">About</Link></li>
+                        <li><Link to="contact">Contact</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="home" element={<Home />}> </Route>
+                    <Route path="about" element={<AboutUs />}> </Route>
+                    <Route path="contact" element={<ContactUs />}> </Route>
+                </Routes>
+
+
+
+            </div>
         </div>
     );
 }
