01c73a4e5bd6301a06437783c27fda0029eb5a08
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-30 00:20:09 +0530

    feat: update Dashboard component for improved routing and add CSS styling

--- day08/src/components/Dashboard.js
+++ day08/src/components/Dashboard.js
@@ -2,6 +2,7 @@ import Home from './Home';
 import ContactUs from './ContactUs';
 import AboutUs from './AboutUs';
 import { Routes, Route, Link } from 'react-router-dom';
+import '../assets/css/dashboard.css'
 
 
 export default function Dashboard() {
@@ -12,15 +13,15 @@ export default function Dashboard() {
 
                 <nav>
                     <ul>
-                        <li><Link to="home">Home</Link></li>
-                        <li><Link to="about">About</Link></li>
-                        <li><Link to="contact">Contact</Link></li>
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="home" element={<Home />}> </Route>
-                    <Route path="about" element={<AboutUs />}> </Route>
-                    <Route path="contact" element={<ContactUs />}> </Route>
+                    <Route path="/home" element={<Home/>}> </Route>
+                    <Route path="/about" element={<AboutUs/>}> </Route>
+                    <Route path="/contact" element={<ContactUs/>}> </Route>
                 </Routes>
 
 
