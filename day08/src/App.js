aaad3d26657d69db15be9b7c8164b02bfedf42cc
Udara Nalawansa <udarakavishka13@gmail.com>
2024-11-29 23:01:18 +0530

    feat: integrate Login component and add Bootstrap styling

--- day08/src/App.js
+++ day08/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      
+      <Login/>
     </div>
   );
 }
